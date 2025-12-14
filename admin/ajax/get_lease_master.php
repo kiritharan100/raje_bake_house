7b72a4fdc2d6bb8ad3da4154f5ce7dde2c069821
Kiritharan <kiritharan100@gmail.com>
2025-12-14 06:36:59 +0530

    Basic Templet

--- admin/ajax/get_lease_master.php
+++
@@ -1,7 +0,0 @@
-<?php
-include '../../db.php';
-include '../../auth.php';
-$id = intval($_POST['lease_type_id']);
-$res = mysqli_query($con, "SELECT * FROM lease_master WHERE lease_type_id=$id");
-$row = mysqli_fetch_assoc($res);
-echo json_encode($row);
